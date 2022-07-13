49f408c7b067e29cc6d3fe50d24a01dba83ea736
nodir <nbotirov2000@gmail.com>
2022-07-13 10:57:57 +0500

    other Id

---
+++ src/main/java/amigos/otherIf.java
@@ -0,0 +1,11 @@
+package amigos;
+
+public class otherIf {
+    public static void main(String[] args) {
+        int age = 18;
+        String message = age >= 18 ?
+                "Hooray... I am an adult" :
+                "I am no an adult";
+        System.out.println(message);
+    }
+}
