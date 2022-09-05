43ed6c36faf9c4dda027f5b04c20a243e77b0ed3
nodir <nbotirov2000@gmail.com>
2022-09-05 11:46:39 +0500

    searchLetter

---
+++ src/main/java/amigos/SearchLetter.java
@@ -0,0 +1,24 @@
+package amigos;
+
+import java.lang.reflect.Method;
+
+public class SearchLetter {
+    public static void main(String[] args) {
+        //Method
+        char[] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D'};
+        int count = countOccurrences(letters, 'C');
+        System.out.println(count);
+    }
+
+    private static int countOccurrences(char[] letters, char searchLetter) {
+        int count =0;
+        for (char letter : letters) {
+            if (letter == searchLetter){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
