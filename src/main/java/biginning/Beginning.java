70c618f6cec1657a8e92ab81862a73821535578a
NodirBotirov <nbotirov2000@gmail.com>
2023-05-18 10:36:29 +0500

    tt

---
+++ src/main/java/biginning/Beginning.java
@@ -0,0 +1,71 @@
+package biginning;
+
+public class Beginning {
+
+
+    public static void main(String[] args) {
+//        String x = "nimadur";
+        int x = 4;
+        int y = 6;
+
+
+//        for (x = 0; x < 10; x++) {
+//            if (x == 1){
+//                System.out.println("privet Andrey " + x);
+//            } else if (x == 2) {
+//                System.out.println("kutakansan");
+//            }
+//        }
+//
+//        while (x<10) {
+//            System.out.println("privet Andrey " + x);
+//        }
+//
+//
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.println(i + " " + j);
+//            }
+//        }
+        float[] arrayOne = new float[]{14, 7, 19, 3, 6, 10};
+        int[] sonlar= new int[]{12, 5, 7, 8, 12};
+        int[] arrays = { 4, 5, 6, 8, 15, 48, 75 };
+
+        int[] arraylar = new  int[]{14, 7, 19, 3, 6, 10};
+
+        System.out.println(arraylar[3]);
+
+        for (int array: arrays){
+            System.out.println(array);
+        }
+
+
+
+
+
+
+//        while (true) {
+//            System.out.println("sss");
+//        }
+
+
+
+//        switch (x){
+//            case "dsfds":
+//                System.out.println("x teng 1");
+//                break;
+//            case "dsfsdf":
+//                System.out.println("x teng 2");
+//                break;
+//            case "nimadur":
+//                System.out.println("x teng nimadurga");
+//                break;
+//            default:
+//                throw new IllegalStateException("Unexpected value: " + x);
+//        }
+
+
+
+
+    }
+}
--- src/main/java/letcode/Parentheses.java
+++ src/main/java/letcode/Parentheses.java
@@ -1,21 +1,21 @@
-package letcode;
-
-public class Parentheses {
-    public static void main(String[] args) {
-
-    }
-    boolean isValid(String s) {
-
-        for(int i=0; i<s.length(); i++){
-            if(
-                    (s[i].equals("(") && s[i+1].equals(")"))
-                            || (s[i].equals("[") && s[i+1].equals("]"))
-                            || (s[i].equals("{") && s[i+1].equals("}"))
-            ){
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
-}
+//package letcode;
+//
+//public class Parentheses {
+//    public static void main(String[] args) {
+//
+//    }
+//    boolean isValid(String s) {
+//
+//        for(int i=0; i<s.length(); i++){
+//            if(
+//                    (s[i].equals("(") && s[i+1].equals(")"))
+//                            || (s[i].equals("[") && s[i+1].equals("]"))
+//                            || (s[i].equals("{") && s[i+1].equals("}"))
+//            ){
+//                return true;
+//            }
+//            return false;
+//        }
+//        return false;
+//    }
+//}
